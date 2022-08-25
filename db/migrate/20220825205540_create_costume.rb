b0818b18cba0cab84857be1f96ee24f039e5a85d
Dennis Kimathi <kimathidennis38@gmail.com>
2022-08-26 00:44:47 +0300

    Done.

--- db/migrate/20220825205540_create_costume.rb
+++ db/migrate/20220825205540_create_costume.rb
@@ -5,8 +5,7 @@ class CreateCostume < ActiveRecord::Migration[6.1]
       t.float :price
       t.string :size
       t.string :image_url
-      t.datetime :created_at
-      t.datetime :updated_at
+      t.timestamps
     end
   end
 end
